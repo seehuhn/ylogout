a105fb2055c26a43d2ac7ccf3b3c623e418b9d0c
Jochen Voss <voss@seehuhn.de>
2013-08-09 11:10:10 +0100

    prepare for a (very belated) public release

--- pixmapdimen.c
+++ pixmapdimen.c
@@ -1,6 +1,21 @@
 /* pixmapdimen.c - get the extents of a X window system pixmap
  *
- * Copyright (C) 1998  Jochen Voss.  */
+ * Copyright (C) 1998, 2013  Jochen Voss
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 
 #include <X11/Xlib.h>
 #include <X11/IntrinsicP.h>
